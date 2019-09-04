32410ef03ef5e0cc1b264dac0bbaeee95267ec4d
Savarin Sitisarn <savarin.sitisarn@gmail.com>
2019-09-04 22:18:20 +0200

    style archive page

--- wp-content/themes/understrap/loop-templates/content-page-products-old.php
+++ wp-content/themes/understrap/loop-templates/content-page-products-old.php
@@ -8,11 +8,44 @@
 		<h6><?php _e('Färska grönsaker kommer på tisdag'); ?></h6>
 		<h6><?php _e('Fresh frozen products and other every Wednesday.'); ?></h6>
 	</div>
-	<div class="items-product">
+	<div class="display">
+
+	<div class="products-new-pop">
 		
 		<?php $terms = get_terms( array(
 			'taxonomy' => 'products_cat',
-			'hide_empty' => false
+			//'hide_empty' => false,
+			'include' => array(85, 87) //display new products cat and popular products cat
+			
+		) ); ?>
+
+		<?php if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
+			
+			foreach ( $terms as $term ) : ?>
+			<div class="new-item">
+			<a href="<?php echo  get_term_link( $term ); ?>">
+			<div class="<?php echo $term->slug; ?> product-new-pop" >
+			</div>
+			<div class="new-title">
+				<li>
+				<?php echo $term->name; ?>		
+				</li>
+			</div>	
+				</a>
+				
+				</div>		
+			
+		<?php endforeach; ?> 
+		<?php } ?>
+
+	</div>
+	
+	<div class="items-product-old">
+		
+		<?php $terms = get_terms( array(
+			'taxonomy' => 'products_cat',
+			//'hide_empty' => false,
+			'exclude' => array(85, 87) //exclude new products cat and popular products cat
 			
 		) ); ?>
 
@@ -20,13 +53,13 @@
 			
 			foreach ( $terms as $term ) : ?>
 			<a href="<?php echo  get_term_link( $term ); ?>">
-			<div class="<?php echo $term->slug; ?> product-item" >
+				<div class="<?php echo $term->slug; ?> product-item-old" >
 			
-				
-				<?php echo $term->name; ?>		
-				
+					<li>
+					<?php echo $term->name; ?>		
+					</li>
 				</div>
-				</a>
+			</a>
 				
 					
 			
@@ -34,3 +67,6 @@
 		<?php } ?>
 
 	</div>
+	</div>
+	
+</div>
